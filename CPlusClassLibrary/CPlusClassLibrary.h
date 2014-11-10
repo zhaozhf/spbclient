9f95d12dc0bcdec29ad3b3ae45cea0bc4c3dbde6
zhaozhf@outlook.com <zhaozhf@outlook.com>
2014-11-10 14:49:17 +0800

    提交

---
+++ CPlusClassLibrary/CPlusClassLibrary.h
@@ -0,0 +1,14 @@
+// CPlusClassLibrary.h
+
+#pragma once
+
+using namespace System;
+
+namespace CPlusClassLibrary {
+
+	public ref class Class1
+	{
+		// TODO:  在此处添加此类的方法。
+		Int32 Add(Int32 a, Int32 b);
+	};
+}
