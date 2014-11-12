ba9405f558d14a0e5effd263a0904436f5fe792d
zhaozhf@outlook.com <zhaozhf@outlook.com>
2014-11-12 14:00:50 +0800

    最终版 KCBPLI_SQLConnect调用报错返回-1

--- KCBPClientTestConsole/Program.cs
+++ KCBPClientTestConsole/Program.cs
@@ -8,41 +8,10 @@ using System.Text;
 
 namespace KCBPClientTestConsole
 {
-    //class Program
-    //{
-    //    //        [DllImport(@"d:\KCBPCli.dll", EntryPoint = "KCBPCLI_Init")]
-    //    [DllImport(@"d:\KCBPCli.dll")]
-
-    //    //"d:\VsCode\KCBPClientTest\KCBPClientTest\KCBPClientTestConsole\bin\Debug\KCBPCli.dll"
-    //    //, EntryPoint = "_Enc7481_Set_Encoder", ExactSpelling = false, CallingConvention = CallingConvention.StdCall)]
-    //    public extern static int KCBPCLI_Init(IntPtr hHandle);
-    //    [DllImport(@"d:\KCBPCli.dll")]
-    //    public extern static int KCBPCLI_GetVersion(IntPtr hHandle, IntPtr pnVersion);
-
-    //    static void Main(string[] args)
-    //    {
-    //        IntPtr hHandle = new IntPtr(0);
-
-    //        IntPtr version = new IntPtr(0);
-    //        //if (KCBPCLI_Init(hHandle) != 0)
-    //        if (KCBPCLI_GetVersion(hHandle, version) != 0)
-    //        {
-    //            Console.WriteLine("KCBPCLI_Init error\n");
-    //            Console.ReadLine();
-    //        }
-
-    //    }
-    //}
 
     unsafe class Program
+    //class Program
     {
-        const int KCBP_SERVERNAME_MAX = 32;
-        const int KCBP_DESCRIPTION_MAX = 32;
-
-
-        [DllImport("Kernel32.dll")]
-        public extern static bool Beep(uint iFreq, uint iDuration);
-
         [DllImport("KCBPCli.dll")]
         public extern static int KCBPCLI_Init(IntPtr hHandle);
 
@@ -52,13 +21,11 @@ namespace KCBPClientTestConsole
         [DllImport("KCBPCli.dll")]
         public extern static int KCBPCLI_SetCliTimeOut(IntPtr hHandle, int TimeOut);
 
-        [DllImport("KCBPCli.dll", CharSet = CharSet.Ansi)]
+        [DllImport("KCBPCli.dll", CharSet = CharSet.Auto)]
         //public extern static int KCBPCLI_SQLConnect(IntPtr hHandle, char* ServerName, char* UserName, char* Password);
         //public extern static int KCBPCLI_SQLConnect(IntPtr hHandle, string ServerName, string UserName, string Password);
         //public extern static int KCBPCLI_SQLConnect(IntPtr hHandle, ref string ServerName, ref string UserName, ref string Password);
-        //public extern static int KCBPCLI_SQLConnect(IntPtr hHandle, string ServerName, string UserName, string Password);
         public extern static int KCBPCLI_SQLConnect(IntPtr hHandle, StringBuilder ServerName, StringBuilder UserName, StringBuilder Password);
-
 
         [DllImport("KCBPCli.dll", CharSet = CharSet.Ansi)]
         public extern static int KCBPCLI_ConnectServer(IntPtr hHandle, string ServerName, string UserName, string Password);
@@ -66,62 +33,51 @@ namespace KCBPClientTestConsole
         public extern static int KCBPCLI_GetErrorCode(IntPtr hHandle, ref int pnErrno);
         [DllImport("KCBPCli.dll", CharSet = CharSet.Unicode)]
         public extern static int KCBPCLI_GetErrorMsg(IntPtr hHandle, string szError);
+        [DllImport("KCBPCli.dll", CharSet = CharSet.Unicode)]
+        public extern static int KCBPCLI_BeginWrite(IntPtr hHandle);
 
-        //[StructLayout(LayoutKind.Sequential)]
-        //public struct tagKCBPConnectOption
-        //{
-        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = KCBP_SERVERNAME_MAX + 1)]
-        //    public char[] szServerName;//= new char[KCBP_SERVERNAME_MAX + 1];
-        //    public uint nProtocal;
-        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = KCBP_DESCRIPTION_MAX + 1)]
-        //    public char[] szAddress;// = new char[KCBP_DESCRIPTION_MAX + 1];
-        //    public uint nPort;
+        [DllImport("KCBPCli.dll", CharSet = CharSet.Unicode)]
+        public extern static int KCBPCLI_SetSystemParam(IntPtr hHandle, int nIndex, string szValue);
 
-        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = KCBP_DESCRIPTION_MAX + 1)]
-        //    public char[] szSendQName;//= new char[KCBP_DESCRIPTION_MAX + 1];
-
-        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = KCBP_DESCRIPTION_MAX + 1)]
-        //    public char[] szReceiveQName;// = new char[KCBP_DESCRIPTION_MAX + 1];
-
-        //    [MarshalAs(UnmanagedType.ByValArray, SizeConst = KCBP_DESCRIPTION_MAX + 1)]
-        //    public char[] szReserved;// = new char[KCBP_DESCRIPTION_MAX + 1];
-        //}
-        [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential, CharSet = System.Runtime.InteropServices.CharSet.Ansi)]
+        [System.Runtime.InteropServices.StructLayoutAttribute(
+            System.Runtime.InteropServices.LayoutKind.Sequential,
+            CharSet = System.Runtime.InteropServices.CharSet.Ansi)]
         public struct tagKCBPConnectOption
         {
-
             /// char[33]
-            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
+            [System.Runtime.InteropServices.MarshalAsAttribute
+                (System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
             public string szServerName;
 
             /// int
             public int nProtocal;
 
             /// char[33]
-            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
+            [System.Runtime.InteropServices.MarshalAsAttribute
+                (System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
             public string szAddress;
 
             /// int
             public int nPort;
 
             /// char[33]
-            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
+            [System.Runtime.InteropServices.MarshalAsAttribute
+                (System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
             public string szSendQName;
 
             /// char[33]
-            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
+            [System.Runtime.InteropServices.MarshalAsAttribute
+                (System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
             public string szReceiveQName;
 
             /// char[33]
-            [System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
+            [System.Runtime.InteropServices.MarshalAsAttribute
+                (System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 33)]
             public string szReserved;
         }
 
-
-
         unsafe static void Main(string[] args)
         {
-
             tagKCBPConnectOption connection = new tagKCBPConnectOption();
             connection.szServerName = "kcbp1";
             connection.nProtocal = 0;
@@ -130,65 +86,77 @@ namespace KCBPClientTestConsole
             connection.szSendQName = "req_cs";
             connection.szReceiveQName = "ans_cs";
 
-            //bool b = Beep(100, 100);
-            //Console.WriteLine(b.ToString());
-
             IntPtr ptr = Marshal.AllocHGlobal(1024);
+
             int resultInit = KCBPCLI_Init(ptr);
+            Console.WriteLine(resultInit.ToString());
 
             int resultCon = KCBPCLI_SetConnectOption(ptr, connection);
+            Console.WriteLine(resultCon.ToString());
 
             int resultTimeOut = KCBPCLI_SetCliTimeOut(ptr, 60);
+            Console.WriteLine(resultTimeOut.ToString());
 
+            string message = string.Format("Connecting {0}:{1}, ReqQ:{2}, AnsQ:{3} ... ...\n",
+               connection.szAddress, connection.nPort,
+               connection.szSendQName, connection.szReceiveQName);
+            Console.WriteLine(message);
+
+            #region 使用char指针
             //char[] szUserName = "KCXP00".ToCharArray();
             //char[] szPassword = "888888".ToCharArray();
-            //char[] serverName = "KCBP01".ToCharArray();
+            //char[] serverName = "kcbp01".ToCharArray();
 
-            //char* d, e, f;
-
+            //int nReturnCode = 0;
             //fixed (char* a = &szUserName[0])
             //fixed (char* b = &szPassword[0])
             //fixed (char* c = &serverName[0])
             //{
-            //    d = a;
-            //    e = b;
-            //    f = c;
+            //    nReturnCode = KCBPCLI_SQLConnect(ptr, c, a, b);
             //}
 
+            //nReturnCode = KCBPCLI_SQLConnect(ptr, &serverName[0], &szUserName[0], &szPassword[0]);
+
+            #endregion
+
+            #region 使用string
             //string szUserName = "KCXP00";
             //string szPassword = "888888";
             //string serverName = "kcbp1";
 
+            //int nReturnCode = KCBPCLI_SQLConnect(ptr, serverName, szUserName, szPassword);
+            #endregion
+
+            #region 使用&string
+            //string szUserName = "KCXP00";
+            //string szPassword = "888888";
+            //string serverName = "kcbp1";
+
+            //int nReturnCode = KCBPCLI_SQLConnect(ptr, ref serverName, ref szUserName, ref szPassword);
+
+            #endregion
+
+            #region 使用StringBuilder
             StringBuilder szUserName = new StringBuilder("KCXP00", 64);
             StringBuilder szPassword = new StringBuilder("888888", 64);
             StringBuilder serverName = new StringBuilder("kcbp1", 32);
 
-
-
-            //int nReturnCode = KCBPCLI_SQLConnect(ptr, &serverName[0], &szUserName[0], &szPassword[0]);
-            //int nReturnCode = KCBPCLI_SQLConnect(ptr, f, d, e);
-            //int nReturnCode = KCBPCLI_SQLConnect(ptr, ref serverName,ref szUserName, ref szPassword);
             int nReturnCode = KCBPCLI_SQLConnect(ptr, serverName, szUserName, szPassword);
-            Console.WriteLine(resultInit.ToString());
-            Console.WriteLine(resultCon.ToString());
-            Console.WriteLine(resultTimeOut.ToString());
-            string message = string.Format("Connecting {0}:{1}, ReqQ:{2}, AnsQ:{3} ... ...\n",
-                connection.szAddress, connection.nPort,
-                connection.szSendQName, connection.szReceiveQName);
-            Console.WriteLine(message);
+            #endregion
+
             string message2 = string.Format("Connecting ServerName:{0},szUserName:{1},szPassword:{2} ... ...\n",
                serverName, szUserName, szPassword);
             Console.WriteLine(message2);
             Console.WriteLine(nReturnCode);
 
-            nReturnCode = 2003;
-            string szErrMsg = "";
-            int x = KCBPCLI_GetErrorCode(ptr, ref nReturnCode);
-            KCBPCLI_GetErrorMsg(ptr, szErrMsg);
+            //nReturnCode = 2003;
+            //string szErrMsg = "";
+            //int ErrorResult = KCBPCLI_GetErrorCode(ptr, ref nReturnCode);
+            //KCBPCLI_GetErrorMsg(ptr, szErrMsg);
 
-            Console.WriteLine(szErrMsg.ToString());
-            Console.WriteLine("x:" + x.ToString());
-            Console.WriteLine(Marshal.PtrToStringAnsi(ptr));
+            //Console.WriteLine(szErrMsg.ToString());
+            //Console.WriteLine("x:" + ErrorResult.ToString());
+            //Console.WriteLine(Marshal.PtrToStringAnsi(ptr));
 
             Console.ReadLine();
             Marshal.FreeHGlobal(ptr);
